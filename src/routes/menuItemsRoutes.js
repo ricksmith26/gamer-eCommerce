6218f460e9857b819f2482036a4b1468b8ee821c
Rick Smith <rick.smith@connectchildcare.com>
2020-05-31 18:55:25 +0100

    updated lifecycles

--- src/routes/menuItemsRoutes.js
+++ src/routes/menuItemsRoutes.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const URL = 'https://master.d3s10xhhiupkua.amplifyapp.com/';
 
 export const getMenuItems = async () => {
+    console.log(res.data, '<<<<<<<RESDATA')
     const res = await axios.get(URL + '/menuItems');
   return res.data;
 };
