f0c542da860a733536cf12d04111bf38cb3a4ecf
Rick Smith <rick.smith@connectchildcare.com>
2020-05-31 19:52:50 +0100

    added headers

--- src/routes/menuItemsRoutes.js
+++ src/routes/menuItemsRoutes.js
@@ -1,7 +1,8 @@
 import client from './client';
 const URL = 'https://sxgtr83iji.execute-api.us-east-1.amazonaws.com/dev';
 
-export const getMenuItems = async () => {
-    const res = await client.get(URL + '/menuItems');
-  return res.data;
+export const getMenuItems = () => {
+	client.get(`${URL}/menuItems`).then(res => {
+		return res.data;
+	});
 };
