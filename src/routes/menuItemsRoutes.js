830f2243cb3f6fadb85eaa659d5f49a4d60af89c
Rick Smith <rick.smith@connectchildcare.com>
2020-05-31 19:11:02 +0100

    changed url

--- src/routes/menuItemsRoutes.js
+++ src/routes/menuItemsRoutes.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const URL = 'https://master.d3s10xhhiupkua.amplifyapp.com/';
+const URL = 'https://sxgtr83iji.execute-api.us-east-1.amazonaws.com/dev';
 
 export const getMenuItems = async () => {
-  const res = await axios.get(URL + '/menuItems');
-  console.log(res.data, '<<<<<<<RESDATA')
+    const res = await axios.get(URL + '/menuItems');
   return res.data;
 };
