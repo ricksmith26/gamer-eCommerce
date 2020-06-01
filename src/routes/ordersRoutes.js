e2462fa0d0d876a3d490cbf1493b80bf67a84e0b
Rick Smith <rick.smith@connectchildcare.com>
2020-06-01 18:40:37 +0100

    updated BE

--- src/routes/ordersRoutes.js
+++ src/routes/ordersRoutes.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:3000/dev';
+const URL = 'https://sxgtr83iji.execute-api.us-east-1.amazonaws.com/dev/';
 
 export const createOrder = async (order) => {
     const res = await axios.post(URL + '/orders/createOrder', order);
